1626530228643be45935e69aa5106dc5c6fab477
gasaferic <gabriele.aloise.2003@gmail.com>
2021-07-20 14:45:31 +0200

    Almost finished bot

--- index.js
+++ index.js
@@ -1,147 +1,69 @@
-const utils = require('./utils')
+const MySQLManager = require('./MySQLManager');
+const BadgeGuild = require('./BadgeGuild');
+var badgeGuilds = {}
+const events = require('events');
 var config = require('./config.json');
+var mysqlConnectionParams = config.mysql;
+const eventEmitter = new events.EventEmitter();
+
 const Discord = require('discord.js');
 const discordButtons = require('discord-buttons');
 const client = new Discord.Client();
 discordButtons(client);
+client.login(config.token)
 
+var mySQLManager = new MySQLManager(mysqlConnectionParams, eventEmitter);
+eventEmitter.once("mysql_connection_ready", function(connectionParams) {
+  console.log("Connesso a " + connectionParams.host + " nel database " + connectionParams.database + " come " + connectionParams.user);
+  badgeGuilds['855928988265873438'] = new BadgeGuild('855928988265873438', { client: client, discordButtons: discordButtons }, mySQLManager)
+  // badgeGuilds['864066342008651818'] = new BadgeGuild('864066342008651818', { client: client, discordButtons: discordButtons }, mySQLManager)
+});
+
+/*
 const gradi = [
-  {name:'Direttore', id:'864124622030241813', inServizio: []},
-  {name:'Vice-Direttore', id:'864124672588251158', inServizio: []},
-  {name:'Primario', id:'864124976582754314', inServizio: []},
-  {name:'Chirurgo', id:'864124875165138984', inServizio: []},
-  {name:'Dottore', id:'864124754210324481', inServizio: []},
-  {name:'Infermiere', id:'864087631361212416', inServizio: []},
-  {name:'Volontario', id:'864124762950991892', inServizio: []}
+  {name:'Direttore', id:'864522265633488897', inServizio: []},
+  {name:'Vice-Direttore', id:'865568626747375617', inServizio: []},
+  {name:'Primario', id:'865568690863079434', inServizio: []},
+  {name:'Chirurgo', id:'865568707484057601', inServizio: []},
+  {name:'Dottore', id:'865568888292507648', inServizio: []},
+  {name:'Infermiere', id:'865568905686548490', inServizio: []},
+  {name:'Volontario', id:'865568929359593483', inServizio: []}
 ]
 
 var currentServer = null;
 var targetChannel = null;
+var logChannel = null;
 var embedMessage = null;
+*/
 
-client.once('ready', () => {
-  console.log('Questo bot è online');
-  currentServer = client.guilds.cache.get('864066342008651818');
-  targetChannel = currentServer.channels.cache.get('864165295777251358')
-  const embedTimbro = new Discord.MessageEmbed()
-	.setColor('#1900ff')
-	.setTitle('Timbro Badge')
-	.setDescription('Ricordatevi di timbrare e stimbrare ogni volta che si entra o si esce dal servizio EMS. Il calcolo delle ore parte da un minimo di 1 ora.')
-	.setThumbnail('https://media.discordapp.net/attachments/864090286119714836/864111861552381997/ems-image.png?width=801&height=547')
-	.setTimestamp()
-  .setFooter('Bot creato da ToxicVolpix#7169 & Gasaferic#8789. Contattateci in privato per qualsiasi problema riscontrato con il bot');
-
-  gradi.forEach((grado) => {
-    embedTimbro.addFields({ name: grado.name, value: "Nessuno in servizio" })
-  })
-  
-  let timbraInButton = new discordButtons.MessageButton()
-  .setLabel("Timbra l'entrata in servizio")
-  .setID("timbraInButton")
-  .setStyle("green")
-  .setEmoji("✔️");
-
-  let timbraOutButton = new discordButtons.MessageButton()
-  .setLabel("Timbra l'uscita dal servizio")
-  .setID("timbraOutButton")
-  .setStyle("red")
-  .setEmoji("✖️");
-
-  let messageActionRow = new discordButtons.MessageActionRow()
-  messageActionRow.addComponents(timbraInButton, timbraOutButton)
-
-  targetChannel.send(embedTimbro, messageActionRow).then(sentMessage => { embedMessage = sentMessage });
-});
-
-
-async  function timbraInButton(button) {
-  var currentRole = utils.getRoleNameById(currentServer.roles.cache, utils.getHighestRole(gradi, button.clicker.member._roles));
-  var currentlyInServizio = utils.getField(gradi, utils.getHighestRole(gradi, button.clicker.member._roles), 'inServizio')
-  if (currentlyInServizio === undefined) { await button.reply.send("Non fai parte dell'ospedale!", true); return; }
-  if (utils.isInServizio(currentlyInServizio, button.clicker.user.id)) { await button.reply.send('Sei già in servizio!', true); return; }
-  utils.updateGradiInServizio(currentlyInServizio, { username: (button.clicker.member.nickname !== null ? button.clicker.member.nickname : button.clicker.user.username), id: button.clicker.user.id }, true)
-  utils.updateMessage(button.message, currentRole, currentlyInServizio);
-  await button.reply.send('Sei entrato in servizio!', true)
-}
-
-async function timbraOutButton(button) {
-  var currentRole = utils.getRoleNameById(currentServer.roles.cache, utils.getHighestRole(gradi, button.clicker.member._roles));
-  var currentlyInServizio = utils.getField(gradi, utils.getHighestRole(gradi, button.clicker.member._roles), 'inServizio')
-  if (currentlyInServizio === undefined) { await button.reply.send("Non fai parte dell'ospedale!", true); return; }
-  if (!utils.isInServizio(currentlyInServizio, button.clicker.user.id)) { await button.reply.send('Non sei in servizio!', true); return; }
-  utils.updateGradiInServizio(currentlyInServizio, { username: (button.clicker.member.nickname !== null ? button.clicker.member.nickname : button.clicker.user.username), id: button.clicker.user.id }, false)
-  utils.updateMessage(button.message, currentRole, currentlyInServizio);
-  await button.reply.send('Sei uscito dal servizio!', true)
-}
-
-client.on('clickButton', async (button) => {
-  if (button.id == "timbraInButton") {
-    timbraInButton(button)
-  } else if (button.id == "timbraOutButton") {
-    timbraOutButton(button)
-  }
-});
-
-client.on("guildMemberUpdate", (oldMember, newMember) => {
-  console.log('cambiati permessi', oldMember, newMember)
-  var currentlyInServizio = utils.getField(gradi, utils.getHighestRole(gradi, oldMember._roles), 'inServizio')
-
-  if (currentlyInServizio === undefined || !utils.isInServizio(currentlyInServizio, oldMember.user.id)) { return; }
-
-  if (oldMember._roles.length !== newMember._roles.length) {
-    var oldRole = utils.getRoleNameById(currentServer.roles.cache, utils.getHighestRole(gradi, oldMember._roles));
-
-    var currentRole = utils.getRoleNameById(currentServer.roles.cache, utils.getHighestRole(gradi, newMember._roles));
-
-    if (oldRole !== currentRole) {
-      utils.updateGradiInServizio(currentlyInServizio, { username: (oldMember.nickname !== null ? oldMember.nickname : oldMember.user.username), id: oldMember.user.id }, false)
-
-      utils.updateMessage(embedMessage, oldRole, currentlyInServizio);
-
-      if (currentRole !== undefined) {
-        currentlyInServizio = utils.getField(gradi, utils.getHighestRole(gradi, newMember._roles), 'inServizio')
-
-        utils.updateGradiInServizio(currentlyInServizio, { username: (newMember.nickname !== null ? newMember.nickname : newMember.user.username), id: newMember.user.id }, true)
-
-        utils.updateMessage(embedMessage, currentRole, currentlyInServizio);
-      }
-    }
-  } else {
-    if (oldMember.nickname !== newMember.nickname) {
-      var currentRole = utils.getRoleNameById(currentServer.roles.cache, utils.getHighestRole(gradi, newMember._roles));
-
-      utils.updateGradiInServizio(currentlyInServizio, { username: (oldMember.nickname !== null ? oldMember.nickname : oldMember.user.username), id: oldMember.user.id }, false)
-      utils.updateGradiInServizio(currentlyInServizio, { username: (newMember.nickname !== null ? newMember.nickname : newMember.user.username), id: newMember.user.id }, true)
-
-      utils.updateMessage(embedMessage, currentRole, currentlyInServizio);
-    }
-  }
-});
-
-/*client.on('messageReactionAdd', (reaction, user) => {
-  if (user.id === reaction.message.author.id) { return; }
-  console.log('id autore', reaction.message.author.id)
-  reaction.message.edit("Sto stronzo de " + user.username + " ha aggiunto na reazione")
-  console.log('content messaggio', reaction.message.author.id)
-  console.log('aggiunta reazione')
-  reaction.users.remove(user.id)
-});*/
-
-client.on('message', msg => {
-  // console.log(msg)
-  if(msg.content === "Buongiorno a tutti"){
-    msg.reply('Sei pronto/a per il servizio in ospedale?');
+client.once("ready", () => {
+  for (var id of client.guilds.cache.keys()) {
+    if (badgeGuilds[id] === undefined) { return; }
+    badgeGuilds[id].clientReady()
   }
 })
 
+client.on('clickButton', async (button) => {
+  // console.log('clickButton', button.guild.id)
+  if (badgeGuilds[button.guild.id] == undefined) { return; }
+  badgeGuilds[button.guild.id].clickButton(button)
+});
+
+client.on("guildMemberUpdate", (oldMember, newMember) => {
+  // console.log('guildMemberUpdate', oldMember.guild.id, newMember.guild.id)
+  if (badgeGuilds[newMember.guild.id] == undefined) { return; }
+  badgeGuilds[newMember.guild.id].guildMemberUpdate(oldMember, newMember)
+});
+
 client.on('message', message => {
-  if(message.content.startsWith('!espelli')){
-    const user = message.mentions.user.first();
-    if(user){
-      const member = message.guild.member(user);
-    }
-    message.reply("L'utente è stato buttato fuori dalla lista con successo!");
+  if (message.content.startsWith('!')) {
+    if (badgeGuilds[message.channel.guild.id] == undefined) { return; }
+    badgeGuilds[message.channel.guild.id].onCommand(message.author.id, message.content, message.channel.id)
+    message.delete();
   }
 });
 
-client.login(config.token)
+client.on("roleUpdate", function(oldRole, newRole){
+  if (badgeGuilds[newRole.guild.id] == undefined) { return; }
+  badgeGuilds[newRole.guild.id].onRoleUpdate(newRole.id);
+});
--- utils.js
+++ utils.js
@@ -1,35 +1,45 @@
-module.exports = {
-  updateField: function (array, field, value) {
+const Discord = require('discord.js');
+var config = require('./config.json');
+
+const fusoOrario = config.fusoOrario
+
+class Utils {
+  constructor() { }
+
+  updateField(array, field, value) {
     array.forEach((elem) => {
       if (elem.name === field) {
         elem.value = value
       }
     })
-    return array
-  },
-  getFieldValue: function (array, field) {
+    return array;
+  }
+
+  getFieldValue(array, field) {
     for (var elem of array) {
       if (elem.name === field) {
         return elem.value
       }
     }
-  },
-  getField: function (array, targetElemID, field) {
+  }
+  getField(array, targetElemID, field) {
     for (var elem of array) {
-      if (elem.id === targetElemID && elem[field] !== undefined) {
+      if (elem.id == targetElemID && elem[field] != undefined) {
         return elem[field]
       }
     }
-  },
-  isInServizio: function (arrayInServizio, id) {
+  }
+
+  isInServizio(arrayInServizio, id) {
     for (var elem of arrayInServizio) {
       if (elem.id === id) {
         return true;
       }
     }
     return false;
-  },
-  removeElemAtIndex: function(array, index) {
+  }
+
+  removeElemAtIndex(array, index) {
     var tempArray = []
 
     for (var elem of array) {
@@ -39,31 +49,46 @@ module.exports = {
     }
 
     return tempArray;
-  },
-  updateGradiInServizio: function (arrayInServizio, utente, add) {
+  }
+
+  updateGradiInServizio(arrayInServizio, utente, add, logChannel) {
     if (add) {
-      arrayInServizio.push({ username: utente.username, id: utente.id, timestampTimbro: new Date().getMilliseconds() })
+      arrayInServizio.push({ username: utente.username, id: utente.id, timestamp: Date.now() })
     } else {
       for (var elem of arrayInServizio) {
         if (elem.id == utente.id) {
+          this.logBadgeOut({ username: '<@' + utente.id + '>', timestamp: { enter: elem.timestamp, exit: Date.now() }, channel: logChannel })
           arrayInServizio.splice(arrayInServizio.indexOf(elem), 1)
           break;
         }
       }
     }
-  },
-  getInServizioListString: function(array) {
+  }
+
+  updateEmbedFields(badgeGuildRoles, guildRoles, message) {
+    badgeGuildRoles.sort((firstRole, secondRole) => guildRoles.get(secondRole.id).rawPosition - guildRoles.get(firstRole.id).rawPosition);
+        
+    message.embeds[0].fields = [];
+    badgeGuildRoles.forEach((ruolo) => {
+      message.embeds[0].addFields({ name: ruolo.name, value: this.getInServizioListString(ruolo.inServizio) });
+    })
+    
+    message.edit(message.embeds[0]);
+  }
+
+  getInServizioListString(array) {
     var listString = "";
-    if (array.length === 0) {
+    if (array.length == 0) {
       listString = "Nessuno in servizio"
     } else {
       for (var elem of array) {
-        listString += elem.username + ((array.indexOf(elem) !== array.length - 1) ? ", " : "")
+        listString += '<@' + elem.id + '>' + ((array.indexOf(elem) !== array.length - 1) ? ", " : "")
       }
     }
     return listString
-  },
-  getHighestRole: function (rootRoles, userRoles) {
+  }
+
+  getHighestRole(rootRoles, userRoles) {
     for (var currentRole of rootRoles) {
       for (var currentUserRoleID of userRoles) {
         if (currentUserRoleID === currentRole.id) {
@@ -71,17 +96,77 @@ module.exports = {
         }
       }
     }
-  },
-  // Gli passo l'id del ruolo dell'utente
-  getRoleNameById: function (rootRoles, id) {
+  }
+
+  getRoleNameById(rootRoles, id) {
     for (const role of rootRoles.keys()) {
       if (role === id) {
         return rootRoles.get(role).name
       }
     }
-  },
-  updateMessage: function (message, role, updatedArray) {
-    message.embeds[0].fields = this.updateField(message.embeds[0].fields, role,this.getInServizioListString(updatedArray))
+  }
+  
+  updateMessageField(message, role, updatedArray) {
+    message.embeds[0].fields = this.updateField(message.embeds[0].fields, role, this.getInServizioListString(updatedArray))
     message.edit(message.embeds[0])
   }
+
+  logBadgeOut(data) {
+    var enterDate = new Date(data.timestamp.enter + (fusoOrario * 60 * 60 * 1000));
+    var exitDate = new Date(data.timestamp.exit+ (fusoOrario * 60 * 60 * 1000));
+    var elapsedTimeFormatted = this.getElapsedTime(data.timestamp.enter, data.timestamp.exit);
+    const infoTimbro = new Discord.MessageEmbed()
+    .setColor('#1900ff')
+    .setTitle('Timbro Badge')
+    .setDescription('Uscita del servizio.')
+    .addFields({ name: "Lavoratore", value: data.username })
+    .addFields({ name: "Inizio servizio", value: enterDate.toUTCString() })
+    .addFields({ name: "Fine servizio", value: exitDate.toUTCString() })
+    .addFields({ name: "Totale ore servizio", value: elapsedTimeFormatted.days + " " + elapsedTimeFormatted.hours + " " + elapsedTimeFormatted.minutes })
+    .setThumbnail('https://media.discordapp.net/attachments/864090286119714836/864111861552381997/ems-image.png?width=801&height=547')
+    .setTimestamp()
+    .setFooter('Bot creato da ToxicVolpix#7169 & Gasaferic#8789. Contattateci in privato per qualsiasi problema riscontrato con il bot');
+    data.channel.send(infoTimbro);
+  }
+
+  getElapsedTime(startMillis, stopMillis) {
+    var elapsedTime = stopMillis - startMillis;
+    var elapsedTimeFormatted = { days: 0, hours: 0, minutes: 0 }
+    elapsedTimeFormatted.minutes = Math.floor((elapsedTime / 1000) / 60);
+    if (elapsedTimeFormatted.minutes > 59) { elapsedTimeFormatted.hours = Math.floor(elapsedTimeFormatted.minutes / 60); elapsedTimeFormatted.minutes = elapsedTimeFormatted.minutes % 60; }
+    if (elapsedTimeFormatted.hours > 23) { elapsedTimeFormatted.days = Math.floor(elapsedTimeFormatted.hours / 24); elapsedTimeFormatted.hours = elapsedTimeFormatted.hours % 24; }
+    elapsedTimeFormatted.days = elapsedTimeFormatted.days == 1 ? elapsedTimeFormatted.days + " Giorno" : elapsedTimeFormatted.days + " Giorni"
+    elapsedTimeFormatted.hours = elapsedTimeFormatted.hours == 1 ? elapsedTimeFormatted.hours + " Ora" : elapsedTimeFormatted.hours + " Ore"
+    elapsedTimeFormatted.minutes = elapsedTimeFormatted.minutes == 1 ? elapsedTimeFormatted.minutes + " Minuto" : elapsedTimeFormatted.minutes + " Minuti"
+    return elapsedTimeFormatted;
+  }
+  // colorHex, title, description, thumbnail, timestamp, footer
+
+  defaultEmbedData = {
+    thumbnail: "https://media.discordapp.net/attachments/864090286119714836/864111861552381997/ems-image.png?width=801&height=547",
+    footer: "Bot creato da ToxicVolpix#7169 & Gasaferic#8789. Contattateci in privato per qualsiasi problema riscontrato con il bot"
+  }
+  
+  getEmbedMessage(embedData) {
+    const embedMessage = new Discord.MessageEmbed();
+    if (embedData.colorHex) { embedMessage.setColor(embedData.colorHex); }
+    if (embedData.title) { embedMessage.setTitle(embedData.title); }
+    if (embedData.description) { embedMessage.setDescription(embedData.description); }
+    if (embedData.timestamp) { embedMessage.setTimestamp(); }
+    embedMessage.setThumbnail(embedData.thumbnail || this.defaultEmbedData.thumbnail);
+    embedMessage.setFooter(embedData.footer || this.defaultEmbedData.footer);
+    return embedMessage;
+  }
+
+  removeEmptyElems(array) {
+    var finalArray = [];
+    for (var elem of array) {
+      if (elem != '') {
+        finalArray.push(elem);
+      }
+    }
+    return finalArray;
+  }
 }
+
+module.exports = Utils
